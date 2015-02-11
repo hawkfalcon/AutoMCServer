0e30abe415f48d42255b51a957920c3c9a483fc9
hawkfalcon <hawkfalcon1@gmail.com>
2015-02-10 16:23:01 -0800

    Initial Commit

---
+++ AutoMCServer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoMCServer
+//
+//  Created by Tristen Miller on 2/10/15.
+//  Copyright (c) 2015 Tristen Miller. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
